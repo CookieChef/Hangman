1a4b248abf5b19ca396cf9062291a8a6bf0b88b9
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-18 17:07:29 -0500

    creating functions

--- assets/game.js
+++ assets/game.js
@@ -1,63 +1,100 @@
 console.log("Thank you for playing!");
-
-//variable declaration for the game scores
-var wins = 0;
-var lost = 0;
-var lettersUsed = [];
-var livesLeft = 10;
-
-
+lettersUsed =[];
 playGame = () => {
-    document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
-    document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed.join(", ");
-    document.querySelector("#wins").innerHTML = "You have guessed " + wins + " words right!";
-    document.querySelector("#lost").innerHTML = "You have died " + lost + " times...";
+    randomWord();
+    userKeyPress();
 
-    //array of words to chose from (animals)
+}
+
+quitGame = () => {
+
+}
+
+//generates a random word
+randomWord = () => {
     var words = ["cat", "dog", "horse", "fish", "piranha", "turtle", "snake", "lion", "tiger", "panda", "aligator",
         "shark", "elephant", "cow", "chicken", "fox", "flamingo", "whale", "lizard", "bear", "sheep", "bird", "wolf",
         "spider", "mosquito", "bee", "raccoon", "bat", "owl", "rabbit", "rat", "mice", "otter", "monkey", "mouse",
         "bobcat", "salamander", "rhinoceros", "opossum", "dolphin", "lynx", "zebra", "iguana", "anteater", "hyena", "koala",
         "jellyfish", "ostrich", "panther", "pigeon", "pig", "reindeer", ""];
 
-    //function that returns a random index from the array, that then becomes the
-    //  word the user needs to guess. 
     var word = words[Math.floor(Math.random() * words.length)];
     console.log("The word to be guessed is: " + word);
-
-    //sets up an array to show how many letters there are in the chosen word by using _'s
+    //makes the space for the randomly chosen word and displays them in the DOM
     var answer = [];
     for (var i = 0; i < word.length; i++) {
         answer[i] = " _ ";
-        console.log(answer);
-        document.querySelector("#spaces").innerHTML = "Your word is:   " + answer;
+        // console.log(answer);
+        document.querySelector("#spaces").innerHTML = "Your word is:   " + (answer.join(" ")); 
     }
-    
-    //======================Game Logic =====================================================
+
+    var remainingLetters = word.length;
+    console.log("remaining letters to be guessed: " + remainingLetters);
+}
+
+//takes the letter the user pressed on and pushes it into an array to display the letters the user already used. 
+userKeyPress = () => {
     document.onkeydown = () => {
         var letter = event.key.toLowerCase();
         lettersUsed.push(letter);
-        updateUserLettersGuessed();
-        var remainingLetters = word.length;
+        document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed.join(", ");
+    }
+}
+//variable declaration for the game scores
+// var wins = 0;
+// var lost = 0;
+// var lettersUsed = [];
+// var livesLeft = 10;
 
 
+// playGame = () => {
+//     lettersUsed=[];
+//     document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
+//     document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed.join(", ");
+//     document.querySelector("#wins").innerHTML = "You have guessed " + wins + " words right!";
+//     document.querySelector("#lost").innerHTML = "You have died " + lost + " times...";
 
-        // while (livesLeft>0 && remainingLetters>0) {
-        //     for (var j=0; j<word.length; j++) {
-        //         if(word[j] === letter) {
-        //             answer[j] = letter; 
-        //             remainingLetters--;
-        //             document.querySelector("#spaces").innerHTML = "Your word is:   " + (answer.join(" "));
-        //         }else if (remainingLetters===0 && livesLeft>0) {
-        //             wins++;
-        //         }else {
-        //             livesLeft--;
-        //             document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
+//     //array of words to chose from (animals)
+//     var words = ["cat", "dog", "horse", "fish", "piranha", "turtle", "snake", "lion", "tiger", "panda", "aligator",
+//         "shark", "elephant", "cow", "chicken", "fox", "flamingo", "whale", "lizard", "bear", "sheep", "bird", "wolf",
+//         "spider", "mosquito", "bee", "raccoon", "bat", "owl", "rabbit", "rat", "mice", "otter", "monkey", "mouse",
+//         "bobcat", "salamander", "rhinoceros", "opossum", "dolphin", "lynx", "zebra", "iguana", "anteater", "hyena", "koala",
+//         "jellyfish", "ostrich", "panther", "pigeon", "pig", "reindeer", ""];
 
-        //         }
-        //     }
-        // }
-        //==================================================================================================
+//     //function that returns a random index from the array, that then becomes the
+//     //  word the user needs to guess. 
+//     var word = words[Math.floor(Math.random() * words.length)];
+//     console.log("The word to be guessed is: " + word);
+
+//     //sets up an array to show how many letters there are in the chosen word by using _'s
+//     var answer = [];
+//     for (var i = 0; i < word.length; i++) {
+//         answer[i] = " _ ";
+//         console.log(answer);
+//         document.querySelector("#spaces").innerHTML = "Your word is:   " + answer;
+//     }
+
+    //======================Game Logic =====================================================
+    // document.onkeydown = () => {
+    //     var letter = event.key.toLowerCase();
+    //     lettersUsed.push(letter);
+    //     updateUserLettersGuessed();
+
+    //     var remainingLetters = word.length;
+
+    //     while (livesLeft>0 && remainingLetters>0) {
+    //         for (var j=0; j<word.length; j++) {
+    //             if(word[j] === letter) {
+    //                 answer[j] = letter; 
+    //                 remainingLetters--;
+    //                 document.querySelector("#spaces").innerHTML = "Your word is:   " + (answer.join(" "));                
+    //             }else if (remainingLetters===0 && livesLeft>0) { 
+    //                 wins++;                  
+    //                 document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
+
+    //             }
+    //         }
+    //     }
     //     //creates a variable to hold the number of remaining letters to be guessed.
     //     var remainingLetters = word.length;
     //     while (remainingLetters>0) {
@@ -79,26 +116,26 @@ playGame = () => {
     //             }
     //         }
     //     }
-        
-    }
-}
+
+//     }
+// }
     //======================================================================================
 
 //function that keeps tack of letters user pressed and displays them on the DOM
-updateUserLettersGuessed = () => {
-    document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed.join(", ");
-}
+// updateUserLettersGuessed = () => {
+//     document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed.join(", ");
+// }
 
-//function that shows users their progress when guessed right
+// //function that shows users their progress when guessed right
 
-quitGame = () => {
-    livesLeft = 10;
-    lettersUsed = [];
-    wins = 0;
-    lost++;
-    document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
-    document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed;
-    document.querySelector("#wins").innerHTML = "You have guessed " + wins + " words right!";
-    document.querySelector("#lost").innerHTML = "You have died " + lost + " times...";
-    playGame();
-}
+// quitGame = () => {
+//     livesLeft = 10;
+//     lettersUsed = [];
+//     wins = 0;
+//     lost++;
+//     document.querySelector("#livesLeft").innerHTML = "You have " + livesLeft + " lives left!";
+//     document.querySelector("#lettersUsed").innerHTML = "You guessed letters: " + lettersUsed;
+//     document.querySelector("#wins").innerHTML = "You have guessed " + wins + " words right!";
+//     document.querySelector("#lost").innerHTML = "You have died " + lost + " times...";
+//     playGame();
+// }
