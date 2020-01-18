17abe7148d41974668616debd84873d49962805c
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-17 22:39:58 -0500

    created array of words

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,7 @@
 console.log("Thank you for playing!");
+
+var words = ["cat", "dog", "horse", "fish", "piranha", "turtle", "snake", "lion", "tiger", "panda", "aligator",
+             "shark", "elephant", "cow", "chicken", "fox", "flamingo", "whale", "lizard", "bear", "sheep", "bird", "wolf",
+            "spider", "mosquito", "bee", "raccoon", "bat", "owl", "rabiit", "rat", "mice", "otter", "monkey", "mouse",
+             "bobcat", "salamander", "rhinoceros", "opossum", "dolphin", "lynx","zebra","iguana","anteater","hyena","koala",
+            "jellyfish","ostrich","panther","pigeon","pig","reindeer",""];
